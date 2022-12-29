b2d59bd9bd07dc3cfb69098049d88b2493a793fc
111 <2310860596@qq.com>
2022-12-29 11:36:34 +0800

    添加默认样式

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  }
+}
 </script>
 
 <style lang="scss">
-/*uview ui */
+/* uview ui */
 @import 'uview-ui/index.scss';
+/* my style */
 @import './style/base.css';
 @import './style/root.css';
+/* #ifdef H5 */
+html {
+  color: var(--text-color-inverse);
+  font-size: var(--font-size-base);
+}
+/* #endif */
+/* #ifndef H5 */
+page {
+  color: var(--text-color-inverse);
+  font-size: var(--font-size-base);
+}
+/* #endif */
 </style>
