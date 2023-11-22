5064223a852a797bafb3d34ef5a78a204a9ed022
CompanyComputers <2310860596@qq.com>
2023-11-22 08:51:17 +0800

    优化代码api

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,11 @@
-export const real = true
-
 let realUrl = ''
 // #ifdef H5
-realUrl = '/api/jnfw'
+realUrl = '/api'
 // #endif
-// #ifndef H5
-realUrl = 'https://szxc.0571ps.com/jnfw'
-// #endif
-const testUrl = 'http://192.168.1.106:3333'
 
-export const baseURL = real ? realUrl : testUrl
+// #ifndef H5
+// 小店经济
+realUrl = process.env.VUE_APP_API_URL
+// #endif
+
+export const baseURL = realUrl
